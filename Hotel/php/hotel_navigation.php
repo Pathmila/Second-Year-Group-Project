72444dad40f4efafd8da2daac541c31768c7b88b
Ruwanthi Hemachandra <56576410+RSH17@users.noreply.github.com>
2020-10-07 10:38:46 +0530

    Update hotel_navigation.php

--- Hotel/php/hotel_navigation.php
+++ Hotel/php/hotel_navigation.php
@@ -11,12 +11,12 @@
         
         <div class="navbar">  
             <a><img src="../images/logo-web.jpg" class="fakeimg"></a>
-            <a href="hotel_home.php"><div class="txt"><Button class="btn active">Home</button></div></a>
-            <a href="hotel_viewprofile.php"><div class="txt"><Button class=" btn">VIEW PROFILE</button></div></a>
-            <a href="hotel_about.php"><div class="txt"><Button class=" btn">ABOUT</button></div></a>
-            <a href="hotel_contact.php"><div class="txt"><Button class=" btn">CONTACT</button></div></a>
-            <a href="hotel_availability.php"><div class="txt"><Button class=" btn">CHANGE AVAILABILITY</button></div></a>
-            <a href="index.php" class="right"><div class="txt"><Button>LOG OUT</button></div></a>
+            <a href="hotel_home.php"><div class="txt"><button class="button">Home</button></div></a>
+            <a href="hotel_viewprofile.php"><div class="txt"><button class=" button">VIEW PROFILE</button></div></a>
+            <a href="hotel_about.php"><div class="txt"><button class=" button">ABOUT</button></div></a>
+            <a href="hotel_contact.php"><div class="txt"><button class=" button">CONTACT</button></div></a>
+            <a href="hotel_availability.php"><div class="txt"><button class=" button">CHANGE AVAILABILITY</button></div></a>
+            <a href="index.php" class="right"><div class="txt"><button>LOG OUT</button></div></a>
         </div>
     </body>
 
