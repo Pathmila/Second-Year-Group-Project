80297e5a5b39c77ee5b3c4f55f2e2f93d076dfe4
Asini Pathmila <56575710+Pathmila@users.noreply.github.com>
2020-10-26 07:36:29 +0530

    Create about.php

---
+++ 3tier/src/php/about.php
@@ -0,0 +1,8 @@
+<?php
+    require_once('connect.php');
+    session_start();
+?>
+<?php require_once('user_nonnavigation.php')?> 
+<?php include('../../public/html/about.html')?>
+<?php require_once('footer.php')?>
+
