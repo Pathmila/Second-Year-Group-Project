3641efffa312b1facd696f1a55b4ba915472c763
Hansaka Sandaruwan <60069417+H-Sandaruwan@users.noreply.github.com>
2020-11-13 17:37:10 +0530

    Add files via upload
    updated

--- Guide/php/guide_change_availability.php
+++ Guide/php/guide_change_availability.php
@@ -14,26 +14,54 @@
 	while($row=$result->fetch_assoc()){
         $uid=(string)$row['uid'];
     }
-	$sql2="select * from guideavailability where gid='".$uid."'";
-	//echo $sql1;
-	$result2=mysqli_query($connection,$sql2);
-	while($row2=$result2->fetch_assoc()){
-        $av=(string)$row2['availability'];
-	}
 ?>
 
 <?php
 	if(isset($_POST['formsubmit'])){
-		$availability=$_POST['availability'];
-		$sql = "Update guideavailability set availability='".$availability."' where gid='".$uid."' ";
+		$day=$_POST['date'];
+		//echo $day;
+		$sql = "Insert into guideavailability(gid,availability_date) values ('".$uid."' , '".$day."') ";
 		//echo $sql;
 		$result=$connection->query($sql);
 		if($result){
 			echo "<script> alert('Update is Sucessfull') </script>";
-			header("Location: guide_home.php");
+			header("Location: guide_change_availability.php");
 		}else{
 			//echo "failed";
 			echo "<script> alert('Update is Failed') </script>";
+		}
+	}
+?>
+<?php
+	if(isset($_POST['formdelete'])){
+		$day=$_POST['deletedate'];
+		
+		$_GLOBAL['ddone']=0;
+		$sql2="select * from guideavailability where gid='".$uid."' and availability_date='".$day."'";
+		//echo $sql2;
+		$result2=mysqli_query($connection,$sql2);
+		while($row2=$result2->fetch_assoc()){
+			$hid=$row2['hid'];			
+			if($hid != null){
+				$_GLOBAL['ddone']=1;
+			}else{
+				$_GLOBAL['ddone']=0;
+			}
+		}
+		if($_GLOBAL['ddone']==1){
+			//echo $day;
+			$sql3 = "delete from guideavailability where gid='".$uid."' and availability_date='".$day."' ";
+			//echo $sql;
+			$result3=$connection->query($sql3);
+			if($result3){
+				echo "<script> alert('Deletion is Sucessfull') </script>";
+				header("Location:guide_change_availability.php");
+			}else{
+				//echo "failed";
+				echo "<script> alert('Deletion is Failed') </script>";
+			}
+		}else{
+			echo "<script> alert('Invalid Date.') </script>";
 		}
 	}
 ?>
